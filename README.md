2d8bed43ef96037af17ed2a1becc0a62daf469fd
Yang Li <idealhack@gmail.com>
2018-07-09 15:03:23 +0800

    use nvALT-tools to generate website
    - add theme
    - add config file
    - add static files
    - add publish script

---
+++ README.md
@@ -0,0 +1,13 @@
+# idealhack’s wiki
+
+📓 personal wiki
+
+## Overview
+
+- Written in [Markdown](https://daringfireball.net/projects/markdown/)
+- Distributed and linked by files
+- Powered by [nvALT](http://brettterpstra.com/projects/nvalt/), [Simplenote](https://simplenote.com/), [Git](https://git-scm.com/) and [nvALT-tools](https://github.com/idealhack/nvALT-tools)
+
+## License
+
+[CC-BY-4.0](https://creativecommons.org/licenses/by/4.0/)
---
+++ publish.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# fork from https://gohugo.io/hosting-and-deployment/hosting-on-github/
+
+DIR=$(dirname "$0")
+
+cd $DIR
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+site-generator
+
+echo "Copying static files"
+cp -r static/* public
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "update"
